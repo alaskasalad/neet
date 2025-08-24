85df49a50ae24f0ceb5de0bcd4e89dd6b1c27cc3
alaskasalad <caitlin.boxx@gmail.com>
2025-08-24 11:54:21 -0400

    first commit

---
+++ arrays/containsDuplicate.py
@@ -0,0 +1,23 @@
+# time: O(n log n)
+# space: O(1)
+ 
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        
+        return False
+
+# time: O(n)
+# space: O(n)    
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
