9ab1d58cd5090d584fc9a89f3ac7721140e5db71
caitlin <113788299+alaskasalad@users.noreply.github.com>
2025-09-18 15:23:07 -0400

    sliding window

--- DSA/arrays/findLen.py
+++ DSA/arrays/findLen.py
@@ -1,2 +1,13 @@
 # valid sub array = sliding window 
 # best valid subarray 
+
+def find_length(nums, k):
+    left = curr = ans = 0
+    for right in range(len(nums)):
+        cur += nums[right]
+        while curr > k:
+            curr -= nums[left]
+            left += 1
+        ans = max(ans, right - left +1)
+
+    return ans 
